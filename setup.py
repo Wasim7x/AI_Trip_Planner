85d58216ddd143cafb657b4e4733dd2ac6ebcc2a
Wasim7x <wasim7x@gmail.com>
2025-09-28 14:09:34 +0530

    project setup and model_loader is completed

--- setup.py
+++ setup.py
@@ -10,11 +10,9 @@ def get_requirements()->List[str]:
     try:
         # Open and read the requirements.txt file
         with open('requirements.txt', 'r') as file:
-            # Read lines from the file
             lines = file.readlines()
             # Process each line
             for line in lines:
-                # Strip whitespace and newline characters
                 requirement = line.strip()
                 # Ignore empty lines and -e .
                 if requirement and requirement != '-e .':
